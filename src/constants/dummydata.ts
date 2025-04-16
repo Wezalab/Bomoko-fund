c003819f25dfc193d3eb5eff4c530d65fc1076bb
guillainbisimwa <guillainbisimwa@hotmail.fr>
2025-04-16 14:45:32 +0300

    Update testimonial's pictures

--- src/constants/dummydata.ts
+++ src/constants/dummydata.ts
@@ -19,32 +19,32 @@ import Img3 from '../assets/targetSupportImg.png'
 
 
 
-export const userTestimonials=[
-    {
-        id:"1",
-        img:testimonialPic1,
-        name:"Kamana John",
-        description:"My go to platform for getting my projects funded "
-    },
-    {
-        id:"2",
-        img:testimonialPic2,
-        name:"Manzi alvin",
-        description:"Thanks to your generous support, we've been able to bring our vision to life!"
-    },
-    {
-        id:"3",
-        img:testimonialPic3,
-        name:"Sudi David",
-        description:"Your support has been truly inspiring! Thanks to your contributions, we’re closer than ever to achieving our goals and creating lasting change"
-    },
-    {
-        id:"4",
-        img:testimonialPic4,
-        name:"Rutayisire Dory",
-        description:"This journey wouldn’t be possible without your generosity and we’re deeply grateful for every donation"
-    },
-]
+export const userTestimonials = [
+  {
+    id: "1",
+    img: testimonialPic1,
+    name: "Nadia CREZI",
+    description: "Ma plateforme de référence pour faire financer mes projets.",
+  },
+  {
+    id: "2",
+    img: testimonialPic2,
+    name: "Sylvie RHÉA",
+    description: "Grâce à votre soutien généreux, nous avons pu concrétiser notre vision !",
+  },
+  {
+    id: "3",
+    img: testimonialPic3,
+    name: "Christian SALUMU",
+    description: "Votre soutien est une vraie source d’inspiration ! Grâce à vos contributions, nous nous rapprochons chaque jour un peu plus de nos objectifs et du changement durable que nous voulons créer.",
+  },
+  {
+    id: "4",
+    img: testimonialPic4,
+    name: "Mme Brigitte",
+    description: "Ce parcours n’aurait pas été possible sans votre générosité. Nous vous remercions profondément pour chaque don.",
+  },
+];
 
 export const projects = [
     {
