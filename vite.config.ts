e665dea45a6aef852941580fedd02a662ff2b293
guillainbisimwa <guillainbisimwa@hotmail.fr>
2025-05-03 23:37:32 +0300

    Remove proxy configuration from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import https from 'https'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,34 +11,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      "/api/auth": {
-        target: "https://46.202.168.1:7008",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api\/auth/, '/api/auth'),
-        agent: new https.Agent({ rejectUnauthorized: false }),
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-          "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-        }
-      },
-      "/api/projects": {
-        target: "https://46.202.168.1:7008",
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api\/projects/, '/api/projects'),
-        agent: new https.Agent({ rejectUnauthorized: false }),
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-          "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-        }
-      },
     },
   },
 })
