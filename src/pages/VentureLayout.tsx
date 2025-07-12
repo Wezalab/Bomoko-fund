576a464218e041415d045493075e200059089ec9
guillainbisimwa <guillainbisimwa@hotmail.fr>
2025-07-12 15:17:46 +0300

    feat: Add VentureLayout component
    - Create layout wrapper for VentureWizard
    - Provide proper page structure for venture creation flow

---
+++ src/pages/VentureLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VentureWizard from '../components/VentureWizard';
+
+const VentureLayout: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <VentureWizard />
+    </div>
+  );
+};
+
+export default VentureLayout; 
