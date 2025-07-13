66226f215eddc11c4acfc3cd74ddc00aa5336ad1
guillainbisimwa <guillainbisimwa@hotmail.fr>
2025-07-13 15:56:07 +0300

    Add Google OAuth provider wrapper to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,21 @@ import { persistor, store } from "./redux/store"
 import { PersistGate } from "redux-persist/integration/react"
 import { TranslationProvider } from "./lib/TranslationContext"
 import { Toaster } from "./components/ui/Toaster"
+import { GoogleOAuthProvider } from '@react-oauth/google'
+import { googleClientId } from "./lib/env"
 
 function App() {
   return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <TranslationProvider>
-          <RootNavigation />
-          <Toaster />
-        </TranslationProvider>
-      </PersistGate>
-    </Provider>
+    <GoogleOAuthProvider clientId={googleClientId}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <TranslationProvider>
+            <RootNavigation />
+            <Toaster />
+          </TranslationProvider>
+        </PersistGate>
+      </Provider>
+    </GoogleOAuthProvider>
   )
 }
 
